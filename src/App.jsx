f3c55e3f19b5bee3bbeada1f94982d348cf9ba35
Valerijxxx <drozdov17@icloud.com>
2024-07-05 12:04:57 +0300

    add-useDebounce

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 
 function App() {
   return (
-  'xui'
+    'xui'
 )
 }
 
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,21 @@
+function useDebounce(value, delay) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+  
+    useEffect(
+      () => {
+        
+        const t = setTimeout(() => {
+          setDebouncedValue(value);
+        }, delay);
+  
+        
+        return () => {
+          clearTimeout(t);
+        };
+      },
+      [value, delay] 
+    );
+    return debouncedValue;
+  }
+  
+  export default useDebounce;
